ebe223d920fc5d8fc9c849660c8ca8249f57c059
Julien Broi <julien.broi@gmail.com>
2025-01-26 23:02:37 +0100

    fix: support scala 3.6.3
    move ZLayer ops to another package to avoid runtime issues with zio and opaque types

---
+++ src/main/scala-3/be/broij/zarrow/ZLayer.scala
@@ -0,0 +1,19 @@
+package be.broij.zarrow.zlayer
+
+import be.broij.zarrow.ZArrow
+import scala.Predef.{identity => id, summon}
+import zio._
+
+final case class Ops[I, R, E, O](zArrow: ZArrow[I, R, E, O]):
+  /**
+   * Constructs a ZLayer from this `ZArrow`.
+   */
+  def layer(using tag: Tag[ZArrow[I, R, E, O]]): ULayer[ZArrow[I, R, E, O]] =
+    ZLayer.succeed(zArrow)
+
+object Ops:
+  /**
+   * Accesses the specified ZArrow in the ZIO environment.
+   */
+  def service[I: Tag, R: Tag, E: Tag, O: Tag] =
+    ZIO.service[ZArrow[I, R, E, O]]
