f2531c820f9bf74898e0f8b78fe5b2f1e5f82736
Julien Broi <julien.broi@gmail.com>
2025-02-01 21:11:53 +0100

    refactor: move tests outside of zarrow package

--- src/test/scala-3/be/broij/ZArrowSpec.scala
+++ src/test/scala-3/be/broij/ZArrowSpec.scala
@@ -12,8 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.*/
 
-package be.broij.zarrow
+package be.broij
 
+import be.broij.zarrow._
 import zio._
 import zio.test._
 import zio.test.Assertion._
